02021e6e037e3f09217305298231ad0719768f6b
Aikar <aikar@aikar.co>
2017-07-03 14:56:00 -0500

    get auto loading locales working

--- src/main/java/co/aikar/locales/LocaleManager.java
+++ src/main/java/co/aikar/locales/LocaleManager.java
@@ -1,20 +1,25 @@
 package co.aikar.locales;
 
-import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
+import org.reflections.Reflections;
+import org.reflections.scanners.ResourcesScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
 
-import javax.annotation.Resource;
-import java.io.File;
-import java.io.FilenameFilter;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.regex.Pattern;
 
-@SuppressWarnings("WeakerAccess")
+@SuppressWarnings({"WeakerAccess", "unused"})
 public class LocaleManager <T> {
-
+    private static volatile Reflections RESOURCE_SCANNER;
+    private static Pattern SPLIT_PATTERN = Pattern.compile("_");
     private final Function<T, Locale> localeMapper;
     private final Locale defaultLocale;
     private final Map<Locale, LanguageTable> tables = new HashMap<>();
@@ -24,40 +29,35 @@ public class LocaleManager <T> {
         this.defaultLocale = defaultLocale;
     }
 
-    public static <T> LocaleManager<T> create(@NonNls Function<T, Locale> localeMapper) {
+    public static <T> LocaleManager<T> create(@NotNull Function<T, Locale> localeMapper) {
         return create(localeMapper, Locale.ENGLISH);
     }
 
-    public static <T> LocaleManager<T> create(@NonNls Function<T, Locale> localeMapper, Locale defaultLocale) {
+    public static <T> LocaleManager<T> create(@NotNull Function<T, Locale> localeMapper, Locale defaultLocale) {
         return new LocaleManager<>(localeMapper, defaultLocale);
     }
 
     public Locale getDefaultLocale() {
         return defaultLocale;
     }
-/*
-    public List<Locale> getResourceBundleLocales(@NotNull String bundleName) {
-        return getResourceBundleLocales(bundleName, "/");
-    }
 
-    public List<Locale> getResourceBundleLocales(@NotNull String bundleName, @NotNull @SuppressWarnings("SameParameterValue") String basePath) {
+    public List<Locale> getResourceBundleLocales(@NotNull String patternPrefix) {
         final ArrayList<Locale> locales = new ArrayList<>();
-        Pattern compile = Pattern.compile("_");
+
         try {
-            URL resource = LocaleManager.class.getResource(basePath);
-            if (resource == null) {
-                return locales;
+            synchronized (LocaleManager.class) {
+                if (RESOURCE_SCANNER == null) {
+                    RESOURCE_SCANNER = new Reflections(new ConfigurationBuilder()
+                            .addUrls(ClasspathHelper.forClassLoader())
+                            .addScanners(new ResourcesScanner()));
+                }
             }
-            File f = new File(resource.toURI());
-            final String bundle = bundleName + "_";
-            FilenameFilter filenameFilter = (dir, name) -> name.startsWith(bundle);
-            String[] list = f.list(filenameFilter);
-            if (list == null) {
-                return locales;
-            }
+            String pattern = patternPrefix + "_.+\\.properties";
+            Set<String> list = RESOURCE_SCANNER.getResources(Pattern.compile(pattern));
+
             for (String s : list) {
-                String substring = s.substring(basePath.length() + bundle.length(), s.indexOf('.'));
-                String[] split = compile.split(substring);
+                String substring = s.substring(patternPrefix.length()+1, s.indexOf('.'));
+                String[] split = SPLIT_PATTERN.split(substring);
                 if (split.length <= 1) {
                     locales.add(new Locale(substring));
                 } else if (split.length == 2) {
@@ -66,13 +66,16 @@ public class LocaleManager <T> {
                     locales.add(new Locale(split[0], split[1], split[2]));
                 }
             }
-        } catch (URISyntaxException x) {
-            throw new RuntimeException(x);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
         locales.trimToSize();
         return locales;
     }
 
+    /**
+     * Scans for every locale available and loads the matching message bundle
+     */
     public boolean addMessageBundles(@NotNull String... bundles) {
         boolean loaded = false;
         for (String bundle : bundles) {
@@ -84,9 +87,16 @@ public class LocaleManager <T> {
         }
 
         return loaded;
-    }*/
+    }
 
+    /**
+     * If a list of locales is supplied, loads the matching message bundle for each locale.
+     * If none are supplied, just the default locale is loaded.
+     */
     public boolean addMessageBundle(@NotNull String bundleName, @NotNull Locale... locales) {
+        if (locales.length == 0) {
+            locales = new Locale[] {defaultLocale};
+        }
         boolean found = false;
         for (Locale locale : locales) {
             if (getTable(locale).addMessageBundle(bundleName)) {
@@ -119,7 +129,7 @@ public class LocaleManager <T> {
         return message;
     }
 
-    public LanguageTable getTable(@NotNull Locale locale) {
+    public @NotNull LanguageTable getTable(@NotNull Locale locale) {
         return tables.computeIfAbsent(locale, LanguageTable::new);
     }
 
